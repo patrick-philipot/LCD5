538ad6a8cb0bfd2eec18f1f99b75d818a423ea41
pat665 <54064246+patrick-philipot@users.noreply.github.com>
2024-10-13 09:31:26 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,48 @@
+const larray = [
+    "",
+    // 1
+    "Possédant un Macintosh SE en état de marche et n'aimant pas les traitements de texte moderne, "+ 
+    "j'ai recherché un équivalent tournant sur ma vieille machine et j'ai découvert WriteNow.",
+    // 2
+    "WriteNow n'a pas cessé de m'étonner par sa rapidité et son ergonomie. Il disposait de toutes " + 
+    "les fonctionnalités que j'attendais et même plus. Je l'ai utilisé pour rédiger Le Club des Cinq en Égypte",
+    // 3
+    "J'ai aussi, par commodité, souvent utilisé un émulateur du Macintosh SE, minivMac du site Gryphel, "+ 
+    "sur mon MacBook Air, notamment dans le train. Le Macintosh SE est lourd, 8 kg ! (voir image suivante)",
+    // 4
+    "Les logiciels MacPaint et HyperCard furent nécessaires pour créer les illustrations, à partir d' " +
+    "images en couleurs converties en N&B par un script en Python.",
+    // 5
+    "WriteNow, MacPaint, HyperCard, tous ces logiciels, quasiment sans bug, furent écrits par des passionnés, " +
+    "aux allures de hippies, mais qui ont littéralement changé le monde.",
+    // 6
+    "La couverture du livre a été composée avec Inkscape, un outil de dessin, puissant et gratuit, produisant " +
+    "des images vectorielles, que l'on peut donc réduire, agrandir, déformer sans perte de qualité.",
+    // 7
+    "WriteNow imprime dans un fichier Postscript (ancêtre du PDF), qui doit être converti en PDF avant " + 
+    "l'impression. On obtient des cahiers de 16 pages, qu'il faut relier entre eux.",
+    // 8
+    "Chaque cahier de 16 pages est constitué de quatre feuilles A4 imprimées au recto et au verso, puis pliées en " + 
+    " leur milieu. La reliure va d'une part solidariser ces feuilles, mais aussi relier chaque cahier au suivant.",
+    // 9
+    "On observe ici, les boucles préparées sur le premier cahier, qui permettront l'arrimage du deuxième cahier. " +
+    "Chaque cahier suivant s'accrochera aux fils reliant les deux cahiers précédant.",
+    // 10
+    "Une aiguille courbée est utilisée pour glisser le fil du cahier en cours sous le lien d'arrimage des deux " + 
+    "cahiers au dessus et ressortir aussitôt.",
+    // 11
+    "Quand tous les cahiers sont reliés, ils sont mis sous presse, puis le dos est encollé pour une meilleure solidité",
+    // 12
+    "Plusieurs couches de colle vinylique blanche sont appliquées successivement. Cette colle restera souple et flexible, " +
+    "même après séchage. Son aspect est alors transparent.",
+    // 13
+    "L'ensemble des cahiers reliés composent le corps de l'ouvrage. Pour la mise au format 12x17 cm, qui est celui de " +
+    "la bibliothèque rose, un cutter bien affuté peut remplacer un massicot.",
+    // 14
+    "La dernière étape consiste à coller le corps de l'ouvrage sur la couverture. Au final, tout compris, " +
+    "impression, pliage des cahiers, reliure, encollage du dos et pose du dos, 48 heures sont un minimum pour " +
+    "obtenir un exemplaire.",
+    // 15
+    "Une carte accompagne le livre. Elle donne des informations sur la localisation d'un prétendu trésor et " +
+    "sert de marque-page. Le sens des hiéroglyphes est donné dans le livre. "  
+];
---
+++ script.js
@@ -0,0 +1,32 @@
+const current = document.querySelector('#current');
+const imgs = document.querySelector('.imgs');
+const img = document.querySelectorAll('.imgs img');
+const opacity = 0.6;
+const legende = document.querySelector("#legende");
+
+// Dialog
+const dialog = document.getElementById("myDialog");
+
+dialog.addEventListener('click', dialClick);
+
+function dialClick() { 
+  dialog.close(); 
+} 
+
+// Texte explicatif
+legende.innerHTML = larray[1];
+
+imgs.addEventListener('click', imgClick);
+
+function imgClick(e) {
+
+  // Change current image to src of clicked image
+  current.src = e.target.src;
+  
+  // Change le texte explicatif
+  index = parseInt(e.target.getAttribute("data-index"))
+  legende.innerHTML = larray[index];
+  // affiche le dialog
+  dialog.showModal();
+
+}
